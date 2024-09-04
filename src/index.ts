5980eec4648c47d45af3d9b27a4ffce8a94453e3
felipy.cabral <felipy.cabral@opah.com.br>
2024-09-03 23:11:40 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export const createPdf = async (filePath: string, options = {}, data = {}) => {
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
+    
     const page = await browser.newPage();
 
     hbs.registerHelper('ifCond', function(
